46187f69c09fb58ec5ddfc943c5ccb77f52e4dbb
Cristian Zalazar <cristian.zalazar@mjus.gba.gob.ar>
2026-02-20 23:04:37 +0000

    Defino las funciones para hacer los fetchs a la API con la BASE_URL definida para no repetirla en cada request

---
+++ app-react/src/api/http.js
@@ -0,0 +1,23 @@
+const BASE_URL = "https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net/api";
+
+export async function httpGet(url) {
+    const response = await fetch(`${BASE_URL}/${path}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+
+export async function httpPost(path, body) {
+    const response = await fetch(`${BASE_URL}/${path}`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
